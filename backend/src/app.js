03e17b24f265b8dd9db666e510f01acd6c7e7ef8
Shivam Shukla <shuklashivam63063@gmail.com>
2026-04-02 16:47:32 +0530

    Code Updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const app = express();
+const todoRoutes = require('./routes/todo.routes')
+const cookieParser = require('cookie-parser')
 
-const todoRoutes = require('./routes/todo.routes');
+app.use(cookieParser())
+app.use(express.json())
+app.use('/api/todos', todoRoutes)
 
-app.use(express.json());
+app.get('/set-cookie', (req, res) => {
+    res.cookie('name', 'user-1');
+    res.send('Cookie has been set');
+})
 
-app.use('/api/todos', todoRoutes);
+app.get('/get-cookie', (req, res) => {
+    res.json(req.cookies);
+})
+
+app.use(session({
+    secret:"mysecretkey",
+    resave: false,
+    saveUninitialized: true,
+}))
 
 module.exports = app;
--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -1,20 +1,41 @@
 const todo = require('../models/todo.model');
-const createTodo = async(req,res)=>{
-    try{
-        const toso = await todo.create(req.body);
-        res.status(200).send("created successfully");
-    } catch (error){
-        res.status(500).json({error : error.message});
+
+const createTodo = async (req, res) => {
+    try {
+        const newTodo = await todo.create(req.body);
+        res.status(200).json({ message: "created successfully", data: newTodo });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
-const getTodos = async(req,res)=>{
-    try{
-        const todo = await Todo.find();
-        res.status(200).send(todo);
-    } catch(error){
-        res.status(500).json({error : error.message});
+const getTodos = async (req, res) => {
+    try {
+        const todos = await todo.find();
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 };
 
-module.exports = {createTodo, getTodos};
+const updateTodos = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedTodo = await todo.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({ message: "todo updated", data: updatedTodo });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await todo.findByIdAndDelete(id);
+        res.status(200).json({ message: "deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = { createTodo, getTodos, updateTodos, deleteTodo };
